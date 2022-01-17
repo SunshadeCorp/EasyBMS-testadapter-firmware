c1239af5600d1359c3c86fdf85800cfa48b15683
derBastler <mail100100101@gmail.com>
2022-01-17 13:17:36 +0100

    Adafruit libraries added

--- EasyBMS-testadapter_software/src/main.cpp
+++ EasyBMS-testadapter_software/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
 
-void setup() {
+
+void setup() 
+{
   // put your setup code here, to run once:
+    pinMode(D6, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
+    digitalWrite(D6, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(D6, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);   
 }

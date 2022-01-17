1dce5005f07d08e30efd554dcd79f03ae7052ad5
derBastler <mail100100101@gmail.com>
2022-01-17 17:15:05 +0100

    fix of the platformio.ini

--- EasyBMS-testadapter_software/src/main.cpp
+++ EasyBMS-testadapter_software/src/main.cpp
@@ -14,7 +14,7 @@ void setup()
 void loop() 
 {
   // put your main code here, to run repeatedly:
-    digitalWrite(D6, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(D6, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(D6, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);   
